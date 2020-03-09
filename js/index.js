373881c734272d76e0a41ac0bc6ccbdfa8e8a6fc
Disasster27 <sf_dis@mail.ru>
2020-03-09 22:23:19 +0300

    comments

--- js/index.js
+++ js/index.js
@@ -26,13 +26,15 @@ document.addEventListener( 'DOMContentLoaded', () => {
    
     class Cube {
         constructor ( size ) {
+            // длина грани
             this.size = size;
             this.zet = 0;
             this.farPlanePoint = [];
             this.nearPlanePoint = [];
             this.mesh = null;
-            this.drawCube ( size );
+            this.drawCube ( this.size );
         }
+        // Mesh для вершин куба
         meshCreate () {
             const sphereGeometry = new THREE.SphereBufferGeometry( 1, 6, 6 );
             const sphereMaterial = new THREE.MeshPhongMaterial( { color: Math.random() * 0xffffff } );
@@ -40,33 +42,40 @@ document.addEventListener( 'DOMContentLoaded', () => {
             sphereMesh.scale.set( 1, 1, 1 );
             return sphereMesh;
         }
-        drawPoint ( x, y , z, e  ) {
+        // отрисовка вершин куба
+        drawPoint ( x, y , z, size  ) {
             this.farPlanePoint = [];
             this.nearPlanePoint = [];
             this.zet = z;   
             this.pointCreate ( x, y, z, this.zet );
-            this.pointCreate ( x, y + e, z, this.zet );
-            this.pointCreate ( x + e, y + e, z, this.zet ); 
-            this.pointCreate ( x + e, y, z, this.zet );
+            this.pointCreate ( x, y + size, z, this.zet );
+            this.pointCreate ( x + size, y + size, z, this.zet ); 
+            this.pointCreate ( x + size, y, z, this.zet );
         
-            this.pointCreate ( x, y, z + e, this.zet );
-            this.pointCreate ( x, y + e, z + e, this.zet );
-            this.pointCreate ( x + e, y + e, z + e, this.zet );
-            this.pointCreate ( x + e, y, z + e, this.zet );
+            this.pointCreate ( x, y, z + size, this.zet );
+            this.pointCreate ( x, y + size, z + size, this.zet );
+            this.pointCreate ( x + size, y + size, z + size, this.zet );
+            this.pointCreate ( x + size, y, z + size, this.zet );
         }
+        // задание координат вершин экземпляра куба
         pointCreate ( x, y, z, zet ) {
             const sphereMesh = this.meshCreate ();
             sphereMesh.position.x = x;
             sphereMesh.position.y = y;
             sphereMesh.position.z = z;
             this.mesh.add( sphereMesh );
+            
             if ( z === zet ) {
+                // массив с координатами дальней плоскости экземпляра куба
                 this.farPlanePoint.push( sphereMesh );
             } else {
+                // массив с координатами ближайшей плоскости экземпляра куба
                 this.nearPlanePoint.push( sphereMesh );
             };
         }
-        draw ( farPlanePoint,nearPlanePoint ) {
+        // отрисовка граней куба
+        drawLine ( farPlanePoint,nearPlanePoint ) {
+            // отрисовка граней между плоскостями
             for ( let i = 0 ; i < arguments[0].length ; i++ ) {
                 const coordinate = [];
                 coordinate.push( new THREE.Vector3( arguments[ 0 ][ i ].position.x, arguments[ 0 ][ i ].position.y, arguments[ 0 ][ i ].position.z ) );
@@ -76,6 +85,7 @@ document.addEventListener( 'DOMContentLoaded', () => {
                 const line = new THREE.Line( geometry, material );
                 this.mesh.add( line );
             };
+            // отрисовка граней ближней и дальней плоскости экземпляра куба
             for ( let g = 0 ; g < arguments.length ; g++  ) {
                 for ( let i = 0 ; i < arguments[g].length ; i++ ) {
                     const coordinate = [];
@@ -91,6 +101,7 @@ document.addEventListener( 'DOMContentLoaded', () => {
                 };
             }; 
         }
+        // Mesh для задания координат экземпляра куба
         mainMeshCreate ( position ) {
             const sphereGeometry = new THREE.SphereBufferGeometry( 1, 6, 6 );
             const sphereMaterial = new THREE.MeshPhongMaterial( { color: Math.random() * 0xffffff } );
@@ -106,92 +117,92 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
             scene.add( this.mesh );
         }
+        // финальная отрисовка экземпляка куба
         drawCube ( size ) {
+            console.log(size)
             this.mainMeshCreate (  );
             this.drawPoint ( 0,0,0,size );
+            this.drawLine( this.farPlanePoint,this.nearPlanePoint );
 
-            this.draw( this.farPlanePoint,this.nearPlanePoint );
-
-        }
-        drawResult ( count ) {
-            for ( let i = 0 ; i < count ; i ++ ) {
-                this.drawCube ( parseInt(Math.random() * 50) + 10 );
-            };
         }
     };
     
-
+    // отрисовка всех кубов 
     function drawResult ( count ) {
         for ( let i = 0 ; i < count ; i ++ ) {
-            new Cube( parseInt(Math.random() * 50) + 10 )
+            new Cube( parseInt(Math.random() * 100) + 10 )
         };
     };
 
-    const gui = new dat.GUI();
+    // изменение колличества рисуемых кубов
+    {
+        const gui = new dat.GUI();
 
-    const controls = {
-        quantity : 10,
-    };
+        const controls = {
+            quantity : 10,
+        };
 
-    let numberOfCubes = gui.add( controls, 'quantity' ).step( 1 );
-    drawResult ( controls.quantity );
-  
-    numberOfCubes.onChange( function( value ){
-        scene.children.length = 1;
+        let numberOfCubes = gui.add( controls, 'quantity' ).step( 1 );
+    
+        numberOfCubes.onChange( function( value ){
+            scene.children.length = 1;
+            drawResult ( controls.quantity );
+        } );
+
         drawResult ( controls.quantity );
-     } );
+    }
 
+    // смена цвета граней по клику на вершину
+    {
+        let raycaster, mouse = new THREE.Vector2();
+        init();
 
+        function init () {
+            raycaster = new THREE.Raycaster();
+            renderer.domElement.addEventListener( 'click', raycast, false );
+            renderer.domElement.addEventListener( 'touchend', raycast, false );
+        };
 
-    let raycaster, mouse = new THREE.Vector2();
-    init();
+        function raycast ( e ) {
+            mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+            mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+            raycaster.setFromCamera( mouse, camera );    
+            let intersects = raycaster.intersectObjects( scene.children, true );
 
-    function init () {
-        raycaster = new THREE.Raycaster();
-        renderer.domElement.addEventListener( 'click', raycast, false );
-        renderer.domElement.addEventListener( 'touchend', raycast, false );
-    };
+            let meshColor = null;
+            let positionCoordinatesString = '';
 
-    function raycast ( e ) {
-        mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-        raycaster.setFromCamera( mouse, camera );    
-        let intersects = raycaster.intersectObjects( scene.children, true );
+            for ( let i = 0; i < intersects.length; i++ ) {
+                if ( intersects[ i ].object.type == 'Mesh' ) {
+                    // цвет вершины 
+                    meshColor = intersects[ i ].object.material.color;
+                    // координаты вершины для сравнения с координатами граней
+                    const positionCoordinates = [];
+                    positionCoordinates.push( intersects[ i ].object.position.x );
+                    positionCoordinates.push( intersects[ i ].object.position.y );
+                    positionCoordinates.push( intersects[ i ].object.position.z );
+                    positionCoordinatesString = positionCoordinates.join();
+                };
 
-        let meshColor = null;
-        let positionCoordinatesString = '';
-
-        for ( let i = 0; i < intersects.length; i++ ) {
-            if ( intersects[ i ].object.type == 'Mesh' ) {
-
-                meshColor = intersects[ i ].object.material.color;
-
-                const positionCoordinates = [];
-
-                positionCoordinates.push( intersects[ i ].object.position.x );
-                positionCoordinates.push( intersects[ i ].object.position.y );
-                positionCoordinates.push( intersects[ i ].object.position.z );
-                positionCoordinatesString = positionCoordinates.join();
-            };
-
-            if ( intersects[ i ].object.type == 'Line' ) {
-                const lineCoordinates = [];
-                intersects[ i ].object.geometry.attributes.position.array.forEach( elem => {
-                        lineCoordinates.push( elem );
-                } );
-
-                const startCoordinates = lineCoordinates.slice( 0, 3 );
-                const endCoordinates = lineCoordinates.slice( 3,  );
-                let startCoordinatesString = startCoordinates.join();
-                let endCoordinatesString = endCoordinates.join();   
-
-                if ( startCoordinatesString === positionCoordinatesString || endCoordinatesString === positionCoordinatesString ) {
-                    intersects[ i ].object.material.color = meshColor;
-                } else { 
+                if ( intersects[ i ].object.type == 'Line' ) {
+                    const lineCoordinates = [];
+                    intersects[ i ].object.geometry.attributes.position.array.forEach( elem => {
+                            lineCoordinates.push( elem );
+                    } );
+                    // получения координат начала и конца грани
+                    const startCoordinates = lineCoordinates.slice( 0, 3 );
+                    const endCoordinates = lineCoordinates.slice( 3,  );
+                    let startCoordinatesString = startCoordinates.join();
+                    let endCoordinatesString = endCoordinates.join();   
+                    // проверка на принадлежность рёбер к вершине
+                    if ( startCoordinatesString === positionCoordinatesString || endCoordinatesString === positionCoordinatesString ) {
+                        // смена цвета
+                        intersects[ i ].object.material.color = meshColor;
+                    };
                 };
             };
         };
-    };
+    }
 
     function loop ( ) {
         TWEEN.update();
